08b804ac44856e3aa4c9abd4ed0af324e6455488
Jason E. Aten, Ph.D. <jason@devnull>
2026-09-06 05:20:47 -0300

    atg. add bounds check optimizations to uint64.go

--- uint64.go
+++ uint64.go
@@ -4,6 +4,7 @@ func putUint64(b []byte, i uint64) {
 	if len(b) < 8 {
 		panicf("putUint64 error: fill-in []byte target must be at least 8 bytes. we saw %v", len(b))
 	}
+	_ = b[7] // early bounds check to guarantee safety of writes below
 	b[0] = byte(i >> 56)
 	b[1] = byte(i >> 48)
 	b[2] = byte(i >> 40)
@@ -18,6 +19,7 @@ func getUint64(b []byte) uint64 {
 	if len(b) < 8 {
 		return 0
 	}
+	_ = b[7] // bounds check hint to compiler; see golang.org/issue/14808
 	return (uint64(b[0]) << 56) | (uint64(b[1]) << 48) |
 		(uint64(b[2]) << 40) | (uint64(b[3]) << 32) |
 		(uint64(b[4]) << 24) | (uint64(b[5]) << 16) |
